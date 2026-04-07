b234ad6554be00f1bfb4dab797d4eb283a0b0bcb
Aleff Mota <quixabaaleff@gmail.com>
2026-04-07 10:37:25 -0300

    Sistema de cadastro

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 const { createApp } = Vue;
+
+
+createApp({
+
+    data () {
+        return {
+            nome: "",
+            email:"",
+            dataNasc: "",
+            CPF:"",
+            senha:"",
+            mostrar: false
+        }
+    },
+    methods: {
+        cadastrarPessoa() {
+
+            if(this.nome === "" || this.email === "" || this.dataNasc === "" || this.CPF === "" || this.senha === "") {
+                alert("Preencha todos os campos para cadastrar!");
+                return
+            } 
+
+            this.mostrar = true
+        }
+    }
+
+}).mount("#app")
